1f433cecfdbc6cc22b49858c750cfa429a3d2bdf
Dima Enns <dieter.enns@gmail.com>
2024-11-28 22:37:06 +0100

    Checking known solutions

--- Main/Days/DayDecoratorPrintSolution.cs
+++ Main/Days/DayDecoratorPrintSolution.cs
@@ -6,11 +6,8 @@ internal class DayDecoratorPrintSolution : IDay, IDecorator<IDay>
 {
     private readonly IDay _inner;
 
-    internal DayDecoratorPrintSolution(IDay inner)
-    {
-        _inner = inner;
-    }
-    
+    internal DayDecoratorPrintSolution(IDay inner) => _inner = inner;
+
     public int Number => _inner.Number;
     public int? SampleNumber => _inner.SampleNumber;
     public string Input => _inner.Input;
@@ -32,8 +29,15 @@ internal class DayDecoratorPrintSolution : IDay, IDecorator<IDay>
 
     private void PrintSolution(string solution, string partLabel)
     {
+        var samplePart = SampleNumber is > 0 ? $".{SampleNumber}" : string.Empty;
+        var taskPart = partLabel == "First" ? "I" : "II";
+        var knownSolution = Solutions.ResourceManager.GetString($"{Number.ToString().PadLeft(2, '0')}{samplePart}{taskPart}");
+        var resultText = knownSolution is null 
+            ? ("?", ConsoleColor.DarkYellow)
+            : solution == knownSolution ? ("\u2713", ConsoleColor.Green) : ("X", ConsoleColor.Red);
         DayHelper.WriteDayLabel(_inner, partLabel);
-        Console.WriteLine(" Solution:");
+        Console.Write(" Solution: ");
+        ConsoleHelper.WriteLineColored(resultText.Item1, resultText.Item2);
         var foregroundColor = solution is Consts.NoSolutionFound or Consts.NothingToDoHere 
             ? ConsoleColor.DarkRed 
             : ConsoleColor.DarkGreen;
