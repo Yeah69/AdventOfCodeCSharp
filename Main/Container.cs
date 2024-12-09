75d1f0b01940d2f0a1052f01b989d0fd4efae55d
Dima Enns <dieter.enns@gmail.com>
2024-12-09 23:39:44 +0100

    Generate unit tests based on known solutions

--- Main/Container.cs
+++ Main/Container.cs
@@ -7,4 +7,6 @@ namespace AdventOfCode;
 [ImplementationChoice(typeof(IDayPicker), typeof(PickLatestOnlyWithSamples))]
 [DecoratorSequenceChoice(typeof(IDay), typeof(IDay), typeof(DayDecoratorTrackTime), typeof(DayDecoratorPrintSolution))]
 [CreateFunction(typeof(IProgram), "Create")]
+[CreateFunction(typeof(Func<long, long?, PickSpecific>), "CreatePickSpecific")]
+[CreateFunction(typeof(IReadOnlyList<IDay>), "CreateDays")]
 internal sealed partial class Container;
--- Main/Days/Pickers/PickSpecific.cs
+++ Main/Days/Pickers/PickSpecific.cs
@@ -1,10 +1,10 @@
 ﻿namespace AdventOfCode.Days.Pickers;
 
-internal class PickSpecific : IDayPicker
+internal class PickSpecific(long number, long? sampleNumber) : IDayPicker
 {
     public IEnumerable<IDay> PickDay(IReadOnlyList<IDay> days) => days
         .Where(d => !string.IsNullOrEmpty(d.Input))
-        .OrderBy(d => d.Number)
-        .SelectMany(d => d.Samples().Append(d))
-        .Where(d => d is { Number: 1, SampleNumber: 1 });
+        .Where(d => d.Number == number)
+        .SelectMany(d => sampleNumber > 0 ? d.Samples().Append(d) : [d])
+        .Where(d => d.SampleNumber == sampleNumber);
 }
