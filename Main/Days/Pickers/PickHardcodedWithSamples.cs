0069241c32103842b14ca1f3c887f0f294f5d7d8
Dima Enns <dieter.enns@gmail.com>
2024-12-20 14:27:47 +0100

    Day 19

---
+++ Main/Days/Pickers/PickHardcodedWithSamples.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Days.Pickers;
+
+internal class PickHardcodedWithSamples : IDayPicker
+{
+    public IEnumerable<IDay> PickDay(IReadOnlyList<IDay> days) => days
+        .Where(d => !string.IsNullOrEmpty(d.Input))
+        .Where(d => d.Number == 19)
+        .SelectMany(d => d.Samples().Append(d));
+}
