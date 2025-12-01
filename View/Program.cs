c4b238b49c16c06e90bae64ee7b24948e2b456cd
Dima Enns <dieter.enns@gmail.com>
2025-12-01 12:10:33 +0100

    Fix clipboard copy from UI

--- View/Program.cs
+++ View/Program.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 
 namespace AdventOfCode.View;
 
@@ -7,6 +8,7 @@ sealed class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
+    [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
 
